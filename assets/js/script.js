77dc288b6e019d4aa3fd40e0fbc3aacab6b5b7eb
allwyns <allwyns@axioned.com>
2022-02-15 18:19:29 +0530

    add: obj created

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+/* create obj start */
+let question_obj = [
+    {"question":"Which one of the following river flows between Vindhyan and Satpura ranges",
+        "option": ["Narmada","Mahanadi","Son","Netravati"],
+        "answer": "Narmada"
+    },
+    {"question":"The Central Rice Research Station is situated in",
+        "option": ["Chennai","Cuttack","Bangalore","Quilon"],
+        "answer": "Cuttack"
+    },
+    {"question":"Who among the following wrote Sanskrit grammar",
+        "option": ["Kalidasa","Charak","Panini","Aryabhatt"],
+        "answer": "Panini"
+    },
+    {"question":"Which among the following headstreams meets the Ganges in last",
+        "option": ["Alaknanda","Pindar","Mandakini","Bhagirathi"],
+        "answer": "Bhagirathi"
+    },
+    {"question":"The metal whose salts are sensitive to light is",
+        "option": ["Zinc","Silver","Copper","Aluminum"],
+        "answer": "Silver"
+    },
+    {"question":"Patanjali is well known for the compilation of",
+        "option": ["Yoga Sutra","Panchatantra","Brahma Sutra","Ayurveda"],
+        "answer": "Yoga Sutra"
+    },
+    {"question":"River Luni originates near Pushkar and drains into which one of the following",
+        "option": ["Rann of Kachchh","Arabian Sea","Gulf of Cambay","Lake Sambhar"],
+        "answer": "Rann of Kachchh"
+    },
+    {"question":"Which one of the following rivers originates in Brahmagiri range of Western Ghats",
+        "option": ["Pennar","Cauvery","Krishna","Tapti"],
+        "answer": "Cauvery"
+    },
+    {"question":"The country that has the highest in Barley Production",
+        "option": ["China","India","Russia","France"],
+        "answer": "Russia"
+    },
+    {"question":"Tsunamis are not caused by",
+        "option": ["Hurricanes","Earthquakes","Undersea landslides","Volcanic eruptions"],
+        "answer": "Hurricanes"
+    },
+];
+
+let answer_obj = [];
+
+/* create obj end */
+
